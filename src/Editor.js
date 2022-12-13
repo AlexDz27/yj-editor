6dc72ad4d804e09538d8d98ce6546c6796f9202c
Alex Dzyuba <alexeydzyuba27@gmail.com>
2022-12-13 17:16:21 +0100

    i

---
+++ src/Editor.js
@@ -0,0 +1,68 @@
+import './editor.css'
+import './row.css'
+import { useRef, useState } from 'react'
+
+function Editor() {
+  return (
+    <section className="editor">
+      <Row placeholder="Write something..." />
+      <button>qwe</button>
+    </section>
+  )
+}
+
+function Row({ placeholder }) {
+  const htmlRef = useRef(null)
+  let textRef = useRef(placeholder)
+  const [isBeingEdited, setIsBeingEdited] = useState(false)
+  const [isHovered, setIsHovered] = useState(false)
+  function handleSettingBackground() {
+    if (isBeingEdited) {
+      handleUnsettingBackground()
+      return
+    }
+    setIsHovered(true)
+  }
+  function handleUnsettingBackground() {  // TODO: r to unsetBackground
+    setIsHovered(false)
+  }
+  function handleBackgroundOnClick() {
+    if (placeholder) return
+    if (textRef.current === '') return
+
+    handleUnsettingBackground()
+  }
+
+  function handleEnter(e) {
+    if (e.key === 'Enter') console.log('Enter!!!!!')
+  }
+
+  let text = placeholder
+  return (
+    <div
+      ref={htmlRef}
+      contentEditable="true"
+      suppressContentEditableWarning="true"
+      onKeyDown={handleEnter}
+      onMouseEnter={handleSettingBackground}
+      onMouseLeave={handleUnsettingBackground}
+      onFocus={handleSettingBackground}
+      onInput={(e) => {
+        handleUnsettingBackground()
+        setIsBeingEdited(true)
+        textRef.current = htmlRef.current.innerText
+      }}
+      onBlur={() => {
+        setIsBeingEdited(false)
+        handleUnsettingBackground()
+      }}
+      onClick={handleBackgroundOnClick}
+      className="row"
+      style={{backgroundColor: isHovered ? '#f0f0f0' : 'initial'}}
+    >
+      {text}
+    </div>
+  )
+}
+
+export default Editor
