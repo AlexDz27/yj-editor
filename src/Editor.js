6660aad70d00033ba5b5fd3f3c01a4c29533a7f5
Alex Dzyuba <alexeydzyuba27@gmail.com>
2023-01-10 18:22:23 +0100

    row sep file

--- src/Editor.js
+++ src/Editor.js
@@ -1,7 +1,6 @@
 import './editor.css'
-import './row.css'
-import { useEffect, useRef, useState } from 'react'
-import { isCaretOnFirstLine, isCaretOnLastLine } from './functions'
+import { useState } from 'react'
+import Row from './Row'
 
 function Editor() {
   let [moreRows, setMoreRows] = useState([])
@@ -23,92 +22,6 @@ function Editor() {
       <Row key={4} placeholder="123123123 1231 123 12 31221 12 3 213" posIdx={4} addRows={addRows} />
       {moreRows.map((r, i) => <Row key={r.key} posIdx={i + 1} addRows={addRows} />)}
     </section>
-  )
-}
-
-function Row({ placeholder, posIdx, addRows }) {
-  const ref = useRef(null)
-  const [isBeingEdited, setIsBeingEdited] = useState(false)
-  const [isHovered, setIsHovered] = useState(false)
-  function setBackground() {
-    if (isBeingEdited) {
-      unsetBackground()
-      return
-    }
-    setIsHovered(true)
-  }
-  function unsetBackground() {
-    setIsHovered(false)
-  }
-
-  function handleEnterAndArrows(e) {
-    unsetBackground()
-
-    if (e.key === 'Enter' && !e.shiftKey) {
-      e.preventDefault()
-      addRows(posIdx)
-    }
-
-    if (e.key === 'ArrowDown') {
-      // going to the 0th char
-      if (!isCaretOnLastLine(ref.current)) return
-
-      e.preventDefault()
-
-      console.log('going down')
-    }
-    if (e.key === 'ArrowUp') {
-      // going to the last char
-      if (!isCaretOnFirstLine(ref.current)) return
-
-      e.preventDefault()
-
-      console.log('going up')
-    }
-  }
-
-  useEffect(() => {
-    if (posIdx === 0) {
-      let range = new Range()
-      range.setStart(ref.current.firstChild, 18)
-      range.setEnd(ref.current.firstChild, 18)
-      document.getSelection().addRange(range)
-      return
-    }
-
-    ref.current.focus()
-  }, [])
-
-  return (
-    <div
-      ref={ref}
-      contentEditable="true"
-      suppressContentEditableWarning="true"
-      onKeyDown={handleEnterAndArrows}
-      onMouseEnter={() => {
-        if (document.activeElement !== ref.current) setBackground()
-      }}
-      onMouseLeave={unsetBackground}
-      onFocus={setBackground}
-      onInput={() => {
-        unsetBackground()
-        setIsBeingEdited(true)
-      }}
-      onBlur={() => {
-        setIsBeingEdited(false)
-        unsetBackground()
-      }}
-      onClick={unsetBackground}
-      onPaste={(e) => {
-        e.preventDefault()
-        const text = e.clipboardData.getData('text/plain')
-        document.execCommand('insertText', false, text)
-      }}
-      className="row"
-      style={{ backgroundColor: isHovered ? '#f0f0f0' : 'initial' }}
-    >
-      {placeholder}
-    </div>
   )
 }
 
---
+++ src/Row.js
@@ -0,0 +1,94 @@
+import './row.css'
+import { useEffect, useRef, useState } from 'react'
+import { isCaretOnFirstLine, isCaretOnLastLine } from './functions'
+
+function Row({ placeholder, posIdx, addRows }) {
+  const ref = useRef(null)
+  const [isBeingEdited, setIsBeingEdited] = useState(false)
+  const [isHovered, setIsHovered] = useState(false)
+  function setBackground() {
+    if (isBeingEdited) {
+      unsetBackground()
+      return
+    }
+    setIsHovered(true)
+  }
+  function unsetBackground() {
+    setIsHovered(false)
+  }
+
+  function handleEnterAndArrows(e) {
+    unsetBackground()
+
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault()
+      addRows(posIdx)
+    }
+
+    if (e.key === 'ArrowDown') {
+      // going to the 0th char
+      if (!isCaretOnLastLine(ref.current)) return
+
+      e.preventDefault()
+
+      console.log('going down')
+    }
+    if (e.key === 'ArrowUp') {
+      // going to the last char
+      if (!isCaretOnFirstLine(ref.current)) return
+
+      e.preventDefault()
+
+      console.log('going up')
+    }
+  }
+
+  useEffect(() => {
+    if (posIdx === 0) {
+      let range = new Range()
+      range.setStart(ref.current.firstChild, 18)
+      range.setEnd(ref.current.firstChild, 18)
+      document.getSelection().addRange(range)
+      return
+    }
+
+    ref.current.focus()
+  }, [])
+
+  return (
+    <div className="outerRow">
+      <span className="dragHandler"></span>
+      <div
+        ref={ref}
+        contentEditable="true"
+        suppressContentEditableWarning="true"
+        onKeyDown={handleEnterAndArrows}
+        onMouseEnter={() => {
+          if (document.activeElement !== ref.current) setBackground()
+        }}
+        onMouseLeave={unsetBackground}
+        onFocus={setBackground}
+        onInput={() => {
+          unsetBackground()
+          setIsBeingEdited(true)
+        }}
+        onBlur={() => {
+          setIsBeingEdited(false)
+          unsetBackground()
+        }}
+        onClick={unsetBackground}
+        onPaste={(e) => {
+          e.preventDefault()
+          const text = e.clipboardData.getData('text/plain')
+          document.execCommand('insertText', false, text)
+        }}
+        className="row"
+        style={{ backgroundColor: isHovered ? '#f0f0f0' : 'initial' }}
+      >
+        {placeholder}
+      </div>
+    </div>
+  )
+}
+
+export default Row
